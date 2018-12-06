6f81a3baa0248a7bf1b47962855b6db86c82d32b
Kiarash Zamanifar <kiarashz@hotmail.com>
2018-12-06 23:01:00 +1100

    initial commit

---
+++ src/main/java/tacos/MytacosApplication.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytacosApplication.class, args);
+	}
+}
