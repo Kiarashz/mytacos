d8a134c0841a1aca94161f202f92af1da84f36de
Kiarash Zamanifar <kiarashz@hotmail.com>
2018-12-20 09:40:58 +1100

    handle post methods

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,40 +4,57 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
 import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
-	
+
 	private final IngredientRepository ingredientRepository;
-	
+	private TacoRepository tacoRepository;
+
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepository) {
+	public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository) {
 		this.ingredientRepository = ingredientRepository;
+		this.tacoRepository = tacoRepository;
+	}
+
+	@ModelAttribute(name = "taco")
+	public Taco taco() {
+		return new Taco();
 	}
 
 	@GetMapping
 	public String design(Model model) {
 		Iterable<Ingredient> iIngredients = ingredientRepository.findAll();
-		for (Type type: Ingredient.Type.values()) {
+		for (Type type : Ingredient.Type.values()) {
 			Stream<Ingredient> sIngredients = StreamSupport.stream(iIngredients.spliterator(), true);
-			model.addAttribute(type.name().toLowerCase(), 
-					sIngredients.filter(ing -> ing.getType().equals(type))
-					.collect(Collectors.toList())
-					);
+			model.addAttribute(type.name().toLowerCase(),
+					sIngredients.filter(ing -> ing.getType().equals(type)).collect(Collectors.toList()));
 		}
-		
+
 		model.addAttribute("taco", new Taco());
 		return "design";
+	}
+
+	@PostMapping
+	public String processDesign(@Valid Taco design, Errors erros) {
+		tacoRepository.save(design);
+		return "redirect:/orders/current";
 	}
 }
