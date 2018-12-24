fe0db787c792e574340b6f0ebada9ea107e151cd
Kiarash Zamanifar <kiarashz@hotmail.com>
2018-12-24 14:13:33 +1100

    Migrated to Gradle
    Workaround Java 11 and Maven issues:
    Could not resolve dependencies for project tutorials.spring5:mytacos:jar:0.0.1-SNAPSHOT: Failure to find org.openjfx:javafx.base:jar:11.0.0-SNAPSHOT
    
    Still issues with Gradle and Lombok but building on STS.

--- src/main/java/tacos/TacosApplication.java
+++ src/main/java/tacos/TacosApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MytacosApplication {
+public class TacosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MytacosApplication.class, args);
+		SpringApplication.run(TacosApplication.class, args);
 	}
+
 }
+
--- src/test/java/tacos/TacosApplicationTests.java
+++ src/test/java/tacos/TacosApplicationTests.java
@@ -7,10 +7,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MytacosApplicationTests {
+public class TacosApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
 }
+
