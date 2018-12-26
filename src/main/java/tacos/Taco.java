f373460afed1925b4857d650eab0cbf18de113b2
Kiarash Zamanifar <kiarashz@hotmail.com>
2018-12-26 12:14:24 +1100

    Fixed validation issue with list size

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -3,6 +3,7 @@ package tacos;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -18,7 +19,7 @@ public class Taco {
 	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
 	
-	@Size(min=1, message="You must choose at least 1 ingredient")
+	@NotEmpty(message="You must choose at least 1 ingredient")
 	private List<Ingredient> ingredients;
 
 }
