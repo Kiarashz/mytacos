79bf97e9aa3ce4041e7596132cae2e735136e600
Kiarash Zamanifar <kiarashz@hotmail.com>
2018-12-28 10:41:49 +1100

    added @SessionAttributes and updated mapping path

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
@@ -15,12 +17,12 @@ import tacos.Order;
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
 	
-	@GetMapping
-	public String orderForm(Model model) {
+	@GetMapping("/current")
+	public String orderForm() {
 		log.info("rendering order form");
-		model.addAttribute("order", new Order());
 		return "orderForm";
 	}
 	
