39a3983e1b19cb7e5e3728636076c804f016eeaf
Kiarash Zamanifar <kiarashz@hotmail.com>
2018-12-28 14:14:48 +1100

    added repository bean

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -2,17 +2,18 @@ package tacos.web;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
+import tacos.data.OrderRepository;
 
 @Slf4j
 @Controller
@@ -20,6 +21,14 @@ import tacos.Order;
 @SessionAttributes("order")
 public class OrderController {
 	
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	public OrderController(OrderRepository orderRepository) {
+		super();
+		this.orderRepository = orderRepository;
+	}
+
 	@GetMapping("/current")
 	public String orderForm() {
 		log.info("rendering order form");
@@ -27,9 +36,14 @@ public class OrderController {
 	}
 	
 	@PostMapping
-	public String processOrder(@Valid Order order, Errors errors) {
+	public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
 		log.info("processing submitted order");
-		return null;
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+		orderRepository.save(order);
+		sessionStatus.setComplete();
+		return "redirect:/";
 	}
 
 }
